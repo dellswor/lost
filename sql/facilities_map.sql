39aefb9edc537615341db2d288672838b59e31d2
dellswor <dellswor@cs.uoregon.edu>
2017-01-28 14:18:01 -0800

    transit file processing complete

--- sql/facilities_map.sql
+++ sql/facilities_map.sql
@@ -3,7 +3,7 @@ INSERT INTO facilities (fcode,common_name) VALUES ('MB005','Moonbase');
 INSERT INTO facilities (fcode,common_name) VALUES ('DC','Washington, DC');
 INSERT INTO facilities (fcode,common_name) VALUES ('HQ','Headquarters');
 INSERT INTO facilities (fcode,common_name) VALUES ('NC','National City');
-INSERT INTO facilities (fcode,common_name) VALUES ('LANM','Los Alamous, NM');
+INSERT INTO facilities (fcode,common_name) VALUES ('LANM','Los Alamos, NM');
 INSERT INTO facilities (fcode,common_name) VALUES ('SPNV','Sparks, NV');
 INSERT INTO facilities (fcode,common_name) VALUES ('S300','Site 300');
 INSERT INTO facilities (fcode,common_name) VALUES ('GRLK','Groom Lake');
--- sql/import_data.sh
+++ sql/import_data.sh
@@ -33,3 +33,13 @@ python3 prep_inv.py all_inventory.csv
 rm all_inventory.csv
 psql $1 -f inv_load.sql
 rm inv_load.sql
+
+# Handle make convoys from the transit data... seems easier than from the convoy data
+python3 norm_tags.py > transit.csv
+# This next step gets tricky, the assets may or may not exist in the data...
+# Would be easier if Postgres supported UPSERT by default.
+python3 do_transit.py $1 $2
+
+# Backfill some asset locations based on the transit records
+
+# Handle the convoy data, add waypoints and missing assets
