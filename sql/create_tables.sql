771a070f3170d3964e103ed6d6cdfe1e397f90ee
Daniel Ellsworth <dellswor@cs.uoregon.edu>
2017-02-10 21:24:12 +0000

    initial assignment 7 create_tables.sql updates

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,39 @@
+/* Table to enumarate valid roles and add the base roles */
+CREATE TABLE roles (
+    role	varchar(32) primary key
+);
+INSERT INTO roles (role) VALUES ('Logistics Officer');
+INSERT INTO roles (role) VALUES ('Facilities Officer');
 
+/* Table to hold the users */
 CREATE TABLE users (
     user_pk	serial primary key, -- symmatically meaningless pk out of habit
     username    varchar(16) unique,        -- 16 chars from spec
     password    varchar(16),        -- Think this will be long enough for hash
-    create_dt   timestamp           -- Will be used to age out accounts
+    create_dt   timestamp,          -- Will be used to age out accounts
+    role        varchar(32) REFERENCES roles(role) not null -- setup role
 );
 
+CREATE TABLE facilities (
+    facility_pk	     serial primary key, -- habit
+    common_name      varchar(32),        -- spec
+    fcode            varchar(6),         -- spec
+    create_dt        timestamp not null,
+    create_user      integer REFERENCES users(user_pk) not null
+);
+
+CREATE TABLE assets (
+    asset_pk         serial primary key, -- habit
+    asset_tag        varchar(16) unique, -- spec, May break if tagging is poorly done
+    description      text,
+    create_dt        timestamp not null,
+    create_user      integer REFERENCES users(user_pk) not null
+);
+
+CREATE TABLE asset_at (
+    asset_fk         integer REFERENCES assets(asset_pk),
+    facility_fk      integer REFERENCES facilities(facility_pk),
+    arrive_dt        timestamp not null,
+    depart_dt        timestamp,
+    disposed_dt      timestamp
+);
