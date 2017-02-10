daf2ced571761d225342bc490466778b86290dbd
Daniel Ellsworth <dellswor@cs.uoregon.edu>
2017-02-10 16:10:41 +0000

    completed step 6

--- src/README.txt
+++ src/README.txt
@@ -5,3 +5,4 @@ config.py - Library code to read the lost configuration file
 templates/
     create_user.html - template for user creation
     error_msg.html   - template for errors outside of the dashboard
+    login.html       - template for the login screen
--- src/app.py
+++ src/app.py
@@ -6,8 +6,34 @@ app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/login',methods=('GET','POST'))
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    if request.method=='POST':
+        if 'username' in request.form and 'password' in request.form:
+            uname = request.form['username']
+            upass = request.form['password']
+            with psycopg2.connect(dbname=dbname,host=dbhost,port=dbport) as conn:
+                cur = conn.cursor()
+                # Checking login
+                sql = "select count(*) from users where username=%s and password=%s"
+                cur.execute(sql,(uname,upass))
+                res = cur.fetchone()[0]
+                if res != 1:
+                    # Username taken
+                    session['error'] = 'Authentication failed for %s.'%uname
+                    return redirect('error')
+                session['username']=uname
+                return redirect('dashboard')
+        session['error'] = 'Invalid form fields'
+        return redirect('error')
+    session['error'] = 'Invalid HTTP method %s'%request.method
+    return redirect('error')
+
+@app.route('/dashboard',methods=('GET',))
+def dashboard():
+    return render_template('dashboard.html',username=session['username'])
 
 @app.route('/error',methods=('GET',))
 def error():
